29193ea4ca87be830f6ec9efad4b40a6bd4c9513
Aibaend <150104013@stu.sdu.edu.kz>
2020-02-19 18:09:40 +0600

    first commit

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/ivahaev/go-xlsx-templater"
 	"github.com/tealeg/xlsx"
+	_ "image/png"
+	"strconv"
 	"strings"
 )
 
@@ -46,24 +50,48 @@ type Company struct {
 	IIN     string `json:"iin,omitempty"`
 }
 
+type Image struct {
+	ID   uint   `json:"id"`
+	Link string `json:"link"`
+}
+type Volume struct {
+	Height  int    `json:"height"`
+	Width   int    `json:"width"`
+	Depth   int    `json:"depth"`
+	Measure string `json:"measure"`
+}
+
 type Product struct {
-	Name       string `json:"name"`
-	Count      uint   `json:"count"`
-	Measure    string `json:"measure"`
-	Price      int    `json:"price"`
-	TotalPrice int    `json:"total_price,omitempty"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Image       *Image  `json:"image"`
+	Count       int     `json:"count"`
+	Measure     string  `json:"measure"`
+	Price       int     `json:"price"`
+	TotalPrice  int     `json:"total_price,omitempty"`
+	Volume      *Volume `json:"volume,omitempty"`
 }
 
 func main() {
+	KPBillReport()
+	KPReport()
+}
 
+func KPBillReport() {
 	excelFileName := "docs/template2.xlsx"
 	xlFile, err := xlsx.OpenFile(excelFileName)
 	if err != nil {
 		return
 	}
-	client := &Company{"MUIT", "This is BIN", "Almaty", "THIS is IIK", "CASPKZ", "This is BIK", "microdistrict Mamyr", "9707074010"}
-	owner := &Company{"KIT SYSTEMS", "This is BIN", "Almaty", "THIS is IIK", "CASPKZ", "This is BIK", "microdistrict Mamyr", "9707074010"}
-	product := &Product{"HDMI", 1, "штук", 1000, 1000}
+	client := &Company{"MUIT", "This is BIN", "Almaty", "THIS is IIK", "CASPKZ",
+		"This is BIK", "This is KBE", "microdistrict Mamyr", "9707074010"}
+	owner := &Company{"KIT SYSTEMS", "This is BIN", "Almaty", "THIS is IIK",
+		"CASPKZ", "This is BIK", "This is KBE", "microdistrict Mamyr", "9707074010"}
+	product := &Product{Name: "HDMI", Count: 1, Measure: "штук", Price: 1000, TotalPrice: 1000}
+	products := []*Product{}
+	products = append(products, product)
+	products = append(products, product)
+
 	agreement := AgreementInfo{Date: "1 сентября 2020", Payment: 1000, Executor: "Andrey", OperationNumber: "XXX13", Agreement: "XXX123", PaymentWord: "тысячи теньге",
 		OwnerCompany: owner, ClientCompany: client}
 	agreement.Products = append(agreement.Products, product)
@@ -91,13 +119,87 @@ func main() {
 			}
 		}
 	}
-	xlFile.Save("docs/result.xlsx")
 
+	xlFile.Save("docs/result.xlsx")
+	//B27
 	f, err := excelize.OpenFile("docs/result.xlsx")
 	if err != nil {
 		println(err.Error())
 		return
 	}
-	rows, err := f.GetRows(sheet)
 
+	y := 27
+	for i := range products {
+		f.SetCellValue(sheet, "B"+strconv.Itoa(y+i), i+1)
+		f.SetCellValue(sheet, "D"+strconv.Itoa(y+i), products[i].Name)
+		f.SetCellValue(sheet, "T"+strconv.Itoa(y+i), products[i].Count)
+		f.SetCellValue(sheet, "X"+strconv.Itoa(y+i), products[i].Measure)
+		f.SetCellValue(sheet, "AA"+strconv.Itoa(y+i), products[i].Price)
+		f.SetCellValue(sheet, "AG"+strconv.Itoa(y+i), products[i].TotalPrice)
+	}
+
+	endRow := 39
+	fixedRow := 13
+	for i := 0; i < fixedRow-len(products); i++ {
+		f.RemoveRow(sheet, endRow-i)
+	}
+
+	f.Save()
+}
+
+type Project struct {
+	Project           string     `json:"project"`
+	Name              string     `json:"name"`
+	DeliverConditions string     `json:"conditions"`
+	PaymentConditions string     `json:"payment_conditions"`
+	Warranty          string     `json:"warranty"`
+	Deadline          string     `json:"deadline"`
+	Products          []*Product `json:"products"`
+}
+
+func KPReport() {
+	f, err := excelize.OpenFile("docs/templateKP.xlsx")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	sheet = "ЛДСП"
+	project := Project{Project: "KIT SYSTEMS", Name: "Hello", DeliverConditions: "упаковка+доставка+монтаж в г. Нур-Султан",
+		PaymentConditions: "наличным ", Warranty: "12 месяцев", Deadline: "р.д. После оплаты и всех согласований."}
+	product := &Product{Name: "Стол", Description: "Этот стол для того чтобы спасти тебя:)", Volume: &Volume{Width: 250, Height: 2260, Depth: 350, Measure: "cm"},
+		Price: 1000000, Count: 10}
+	products := []*Product{}
+	products = append(products, product)
+	products = append(products, product)
+	fmt.Print(len(products))
+
+	f.SetCellValue(sheet, "C4", project.Project)
+	f.SetCellValue(sheet, "C5", project.Name)
+	f.SetCellValue(sheet, "C6", project.DeliverConditions)
+	f.SetCellValue(sheet, "C7", project.PaymentConditions)
+	f.SetCellValue(sheet, "C8", project.Warranty)
+	f.SetCellValue(sheet, "C10", project.Deadline)
+
+	starter := 15
+	for i := range products {
+
+		f.SetCellValue(sheet, "A"+strconv.Itoa(starter+i), i+1)
+		f.SetCellValue(sheet, "B"+strconv.Itoa(starter+i), product.Name+" "+strconv.Itoa(product.Volume.Width)+"X"+
+			strconv.Itoa(product.Volume.Height)+"X"+strconv.Itoa(product.Volume.Depth))
+		//image paste
+		err = f.AddPicture(sheet, "C"+strconv.Itoa(starter+i), "images/table2.png", `{"x_scale": 0.5, "y_scale": 0.5}`)
+		if err != nil {
+			fmt.Println(err)
+		}
+		f.SetCellValue(sheet, "D"+strconv.Itoa(starter+i), product.Description)
+		f.SetCellValue(sheet, "E"+strconv.Itoa(starter+i), product.Volume.Width)
+		f.SetCellValue(sheet, "F"+strconv.Itoa(starter+i), product.Volume.Depth)
+		f.SetCellValue(sheet, "G"+strconv.Itoa(starter+i), product.Volume.Height)
+		f.SetCellValue(sheet, "H"+strconv.Itoa(starter+i), product.Price)
+		f.SetCellValue(sheet, "I"+strconv.Itoa(starter+i), product.Count)
+		f.SetCellValue(sheet, "J"+strconv.Itoa(starter+i), product.Price*product.Count)
+
+	}
+
+	f.SaveAs("docs/resultKP.xlsx")
 }
