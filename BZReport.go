b687e3f2c0ec42a5a43fc7e8b3ff1e07bdfb9abb
Aibaend <150104013@stu.sdu.edu.kz>
2020-02-20 18:27:56 +0600

    BZ begining

---
+++ BZReport.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"strconv"
+)
+
+type Order struct {
+	Aim          string `json:"aim"`
+	AcceptDate   string `json:"accept_date"`
+	СustomerName string `json:"сustomer_name"`
+}
+
+func BZReport() {
+	f, err := excelize.OpenFile("docs/templateBZ.xlsx")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	sheet = "БЗ2"
+	f.SetCellValue(sheet, "E4", "Number of BZ")   // номер бланка заказа
+	f.SetCellValue(sheet, "C6", "Aim of BZ")      //Назначение
+	f.SetCellValue(sheet, "C8", "Due date")       //Дата постпуление заказа
+	f.SetCellValue(sheet, "C20", "Customer")      // Заказчик
+	f.SetCellValue(sheet, "C14", "Customer info") //ФИО и тел. конт. лица
+	f.SetCellValue(sheet, "C16", "Work days")     //Дни рабочие по КП либо Договору
+	f.SetCellValue(sheet, "C17", "Address")       //Address
+
+	f.SetCellValue(sheet, "J26", "Material Info")   //Материал основы/толщины
+	f.SetCellValue(sheet, "J27", "Material Info")   //Материал отделки/вид/цвет
+	f.SetCellValue(sheet, "J28", "Material Info")   //Покрытие
+	f.SetCellValue(sheet, "J29", "Material Info")   //Кромка/толщина/ цвет
+	f.SetCellValue(sheet, "I30", "Material Info")   //Цвет, вид, код ручки
+	f.SetCellValue(sheet, "E31", "Additional Info") //Доп., информация:
+
+	starter := 21
+
+	var array []int
+	for i := range array {
+		f.SetCellValue(sheet, "A"+strconv.Itoa(starter+i), i+1)
+		f.SetCellValue(sheet, "B"+strconv.Itoa(starter+i), "Name")         //Наименования
+		f.SetCellValue(sheet, "E"+strconv.Itoa(starter+i), "Number of TX") //Номер ТХ
+		f.SetCellValue(sheet, "G"+strconv.Itoa(starter+i), "Weight")       //Ширина
+		f.SetCellValue(sheet, "H"+strconv.Itoa(starter+i), "Depth")        //Глубина
+		f.SetCellValue(sheet, "I"+strconv.Itoa(starter+i), "Height")       //Высота
+		f.SetCellValue(sheet, "J"+strconv.Itoa(starter+i), "Count")        //Количество
+		f.SetCellValue(sheet, "K"+strconv.Itoa(starter+i), "Price")        // Цена
+		f.SetCellValue(sheet, "L"+strconv.Itoa(starter+i), "SUM")          //Сумма
+	}
+
+	f.SaveAs("docs/resultBZ.xlsx")
+}
---
+++ KPBill.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/tealeg/xlsx"
+	"strconv"
+	"strings"
+)
+
+var sheet = "Лист1"
+var company = "{my_company_name}"
+var bin = "{BIN}"
+var iik = "{iik}"
+var kbe = "{kbe}"
+var bank = "{BANK}"
+var bik = "{BIK}"
+var payment_code = "{code_of_payment}"
+var operation_number = "{operation_number}"
+var address = "{operation_number}"
+var city = "{city}"
+var agreement = "{agreement}"
+var payment = "{payment}"
+var paymentword = "{payment_word}"
+var executor = "{executor}"
+
+type AgreementInfo struct {
+	OwnerCompany    *Company   `json:"owner_company"`
+	ClientCompany   *Company   `json:"client_company"`
+	Date            string     `json:"date"`
+	OperationNumber string     `json:"operation_number"`
+	Agreement       string     `json:"agreement"`
+	Payment         int        `json:"payment"`
+	PaymentWord     string     `json:"payment_word"`
+	Executor        string     `json:"executor"`
+	Products        []*Product `json:"products"`
+}
+
+type Company struct {
+	Name    string
+	BIN     string `json:"bin"`
+	City    string `json:"city"`
+	IIK     string `json:"iik"`
+	Bank    string `json:"bank"`
+	BIK     string `json:"bik"`
+	KBE     string `json:"kbe"`
+	Address string `json:"address"`
+	IIN     string `json:"iin,omitempty"`
+}
+
+type Image struct {
+	ID   uint   `json:"id"`
+	Link string `json:"link"`
+}
+type Volume struct {
+	Height  int    `json:"height"`
+	Width   int    `json:"width"`
+	Depth   int    `json:"depth"`
+	Measure string `json:"measure"`
+}
+
+type Product struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Image       *Image  `json:"image"`
+	Count       int     `json:"count"`
+	Measure     string  `json:"measure"`
+	Price       int     `json:"price"`
+	TotalPrice  int     `json:"total_price,omitempty"`
+	Volume      *Volume `json:"volume,omitempty"`
+}
+
+func KPBillReport() {
+	excelFileName := "docs/template2.xlsx"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		return
+	}
+	client := &Company{"MUIT", "This is BIN", "Almaty", "THIS is IIK", "CASPKZ",
+		"This is BIK", "This is KBE", "microdistrict Mamyr", "9707074010"}
+	owner := &Company{"KIT SYSTEMS", "This is BIN", "Almaty", "THIS is IIK",
+		"CASPKZ", "This is BIK", "This is KBE", "microdistrict Mamyr", "9707074010"}
+	product := &Product{Name: "HDMI", Count: 1, Measure: "штук", Price: 1000, TotalPrice: 1000}
+	products := []*Product{}
+	products = append(products, product)
+	products = append(products, product)
+
+	agreement := AgreementInfo{Date: "1 сентября 2020", Payment: 1000, Executor: "Andrey", OperationNumber: "XXX13", Agreement: "XXX123", PaymentWord: "тысячи теньге",
+		OwnerCompany: owner, ClientCompany: client}
+	agreement.Products = append(agreement.Products, product)
+
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				if strings.Contains(cell.String(), company) {
+					result := strings.Replace(cell.String(), company, agreement.ClientCompany.Name, 1)
+					cell.Value = result
+				}
+				if strings.Contains(cell.String(), bin) {
+					result := strings.Replace(cell.String(), bin, agreement.ClientCompany.BIN, 1)
+					cell.Value = result
+				}
+				if strings.Contains(cell.String(), iik) {
+					result := strings.Replace(cell.String(), iik, agreement.ClientCompany.IIK, 1)
+					cell.Value = result
+				}
+				if strings.Contains(cell.String(), kbe) {
+					result := strings.Replace(cell.String(), kbe, agreement.ClientCompany.KBE, 1)
+					cell.Value = result
+				}
+
+			}
+		}
+	}
+
+	xlFile.Save("docs/result.xlsx")
+	//B27
+	f, err := excelize.OpenFile("docs/result.xlsx")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	y := 27
+	for i := range products {
+		f.SetCellValue(sheet, "B"+strconv.Itoa(y+i), i+1)
+		f.SetCellValue(sheet, "D"+strconv.Itoa(y+i), products[i].Name)
+		f.SetCellValue(sheet, "T"+strconv.Itoa(y+i), products[i].Count)
+		f.SetCellValue(sheet, "X"+strconv.Itoa(y+i), products[i].Measure)
+		f.SetCellValue(sheet, "AA"+strconv.Itoa(y+i), products[i].Price)
+		f.SetCellValue(sheet, "AG"+strconv.Itoa(y+i), products[i].TotalPrice)
+	}
+
+	endRow := 39
+	fixedRow := 13
+	for i := 0; i < fixedRow-len(products); i++ {
+		f.RemoveRow(sheet, endRow-i)
+	}
+
+	f.Save()
+}
